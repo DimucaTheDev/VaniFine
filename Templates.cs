43916960a78693f4f24bf84d59a4247bcdd72501
DimucaTheDev <dima.punchenko@gmail.com>
2025-05-23 22:51:49 +0300

    Extracted WinAPI methods and Templates to another classes

---
+++ Templates.cs
@@ -0,0 +1,52 @@
+﻿namespace VaniFine
+{
+    internal static class Templates
+    {
+        public static readonly string DefinitionTemplate =
+            """
+            {
+                "model": {
+                    "type": "minecraft:select",
+                    "property": "minecraft:component",
+                    "component": "NBT_NAME",
+                    "cases": [
+                        CASES
+                    ],
+                    "fallback": {
+                        FALLBACK
+                    }
+                }
+            }
+            """;
+
+        public static readonly string CaseTemplate =
+            """
+            {
+                "model": {
+                    "type": "minecraft:model",
+                    "model": "minecraft:MODEL"
+                },
+                "when": WHEN
+            }
+            """;
+
+        public static readonly string EmptyCaseTemplate =
+            """
+            {
+                "model": {
+                    "type": "minecraft:model",
+                    "model": "minecraft:item/ITEM"
+                }
+            }
+            """;
+        public static readonly string SampleItemModelTemplate =
+            """
+            {
+                "parent": "item/generated",
+                "textures": {
+                    "layer0": "item/ITEM"
+                }
+            }
+            """;
+    }
+}
